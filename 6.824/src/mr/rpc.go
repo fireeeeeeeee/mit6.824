5755a16ec115faff51e7fb5614560b3143b98569
fireeeeeeeee <879719464@qq.com>
2021-12-22 19:02:57 +0800

    first commit

---
+++ 6.824/src/mr/rpc.go
@@ -0,0 +1,55 @@
+package mr
+
+//
+// RPC definitions.
+//
+// remember to capitalize all names.
+//
+
+import (
+	"os"
+	"strconv"
+)
+
+//
+// example to show how to declare the arguments
+// and reply for an RPC.
+//
+
+type ExampleArgs struct {
+	X int
+}
+
+type ExampleReply struct {
+	Y int
+}
+
+type OP int
+
+const (
+	MAPOP    OP = 1
+	REDUCEOP OP = 2
+	NONEOP   OP = 3
+)
+
+type WorkRequest struct {
+	FILENAME  string
+	REQUESTOP OP
+}
+
+type WorkReply struct {
+	FILENAME string
+	REPLYOP  OP
+}
+
+// Add your RPC definitions here.
+
+// Cook up a unique-ish UNIX-domain socket name
+// in /var/tmp, for the master.
+// Can't use the current directory since
+// Athena AFS doesn't support UNIX-domain sockets.
+func masterSock() string {
+	s := "/var/tmp/824-mr-"
+	s += strconv.Itoa(os.Getuid())
+	return s
+}
---
+++ 6.824/src/mr/worker.go
@@ -0,0 +1,122 @@
+package mr
+
+import (
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"net/rpc"
+	"os"
+	"time"
+)
+
+//
+// Map functions return a slice of KeyValue.
+//
+type KeyValue struct {
+	Key   string
+	Value string
+}
+
+//
+// use ihash(key) % NReduce to choose the reduce
+// task number for each KeyValue emitted by Map.
+//
+func ihash(key string) int {
+	h := fnv.New32a()
+	h.Write([]byte(key))
+	return int(h.Sum32() & 0x7fffffff)
+}
+
+//
+// main/mrworker.go calls this function.
+//
+func Worker(mapf func(string, string) []KeyValue,
+	reducef func(string, []string) string) {
+
+	wRequest := WorkRequest{REQUESTOP: NONEOP}
+	wReply := WorkReply{}
+	call("Master.Request", &wRequest, &wReply)
+
+	switch wReply.REPLYOP {
+	case MAPOP:
+		{
+			filename := wReply.FILENAME
+			file, err := os.Open(filename)
+			if err != nil {
+				log.Fatalf("cannot open %v", filename)
+			}
+			content, err := ioutil.ReadAll(file)
+			if err != nil {
+				log.Fatalf("cannot read %v", filename)
+			}
+			file.Close()
+			kva := mapf(filename, string(content))
+			fmt.Println(kva)
+		}
+	case REDUCEOP:
+		{
+
+		}
+	case NONEOP:
+		{
+			time.Sleep(time.Second)
+		}
+	default:
+		{
+			log.Fatal("unkonwn op:", wReply.REPLYOP)
+		}
+
+	}
+
+	// Your worker implementation here.
+
+	// uncomment to send the Example RPC to the master.
+	//CallExample()
+
+}
+
+//
+// example function to show how to make an RPC call to the master.
+//
+// the RPC argument and reply types are defined in rpc.go.
+//
+func CallExample() {
+
+	// declare an argument structure.
+	args := ExampleArgs{}
+
+	// fill in the argument(s).
+	args.X = 99
+
+	// declare a reply structure.
+	reply := ExampleReply{}
+
+	// send the RPC request, wait for the reply.
+	call("Master.Example", &args, &reply)
+
+	// reply.Y should be 100.
+	fmt.Printf("reply.Y %v\n", reply.Y)
+}
+
+//
+// send an RPC request to the master, wait for the response.
+// usually returns true.
+// returns false if something goes wrong.
+//
+func call(rpcname string, args interface{}, reply interface{}) bool {
+	//c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
+	sockname := masterSock()
+	c, err := rpc.DialHTTP("unix", sockname)
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+	defer c.Close()
+	err = c.Call(rpcname, args, reply)
+	if err == nil {
+		return true
+	}
+
+	fmt.Println(err)
+	return false
+}
